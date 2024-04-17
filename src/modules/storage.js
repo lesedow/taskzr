3438b38e81611951b85bb300a8f76e34b2edba8b
lesedow <lesedow@gmail.com>
2024-04-17 12:02:44 +0300

    add new functions
    A function to retrieve today's tasks
    A function to edit a specific task
    A function to create a new task
    A function to get a task index by its id
    A function to get a task by id

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -15,11 +15,12 @@ export default class Storage {
 
 	static #createDefaultProject() {
 		const inbox = Object.assign(new Project('Inbox'), ProjectMethods());
-		const today = format(Date.now(), 'MM/dd/yyyy'); 
+		const today = format(Date.now(), 'yyyy-MM-dd'); 
 
-		const defaultTask = new Task('Your first task', 'This is your first task.', 4, today);
+		const defaultTask = new Task(inbox.id, 'Your first task', 'This is your first task.', 4, today);
 
 		inbox.addTask(defaultTask);
+
 		Storage.addProject(inbox);
 		Storage.saveProjectsToStorage();
 	}
@@ -39,7 +40,7 @@ export default class Storage {
 	}
 
 	static getTodayTasksCount() {
-		const todayDate = format(Date.now(), 'MM/dd/yyyy');
+		const todayDate = format(Date.now(), 'yyyy-MM-dd');
 		let counter = 0;
 
 		Storage.projects.forEach(project => {
@@ -49,8 +50,50 @@ export default class Storage {
 		return counter;
 	}
 
+	static getTodayTasks() {
+		const todayDate = format(Date.now(), 'yyyy-MM-dd');
+
+		return Storage.projects.reduce((today, project) => {
+			let tasksForToday = project.tasks.filter(task => isEqual(task.dueDate, todayDate));
+			today = today.concat(tasksForToday);
+
+			return today;
+		}, []);
+	}
+
 	static getProjectNumberOfTasks(id) {
 		return Storage.getProjectById(id).tasks.length;
+	}
+
+	static editTask(projectId, taskId, properties) {
+		let project = Storage.getProjectById(projectId);
+		let taskIndex = Storage.getTaskIndexById(project, taskId);
+
+		let editedTask = new Task(...properties, taskId);
+		project[taskIndex] = editedTask;
+		
+		// task.title = title || task.title;
+		// task.description = description || task.description;
+		// task.priority = priority || task.priority;
+		// task.dueDate = dueDate || task.dueDate;
+		// task.title = completed || task.completed;
+
+		Storage.saveProjectsToStorage();
+	}
+
+	static createNewTask(properties) {
+		const newTask = new Task(properties);
+		Storage.addTaskToProject(newTask, properties.projectID);
+	}
+
+	static getTaskIndexById(project, taskId) {
+		return project.findIndex((task) => task.id === taskId);
+	}
+
+	static getTaskById(projectId, taskId) {
+		const project = Storage.getProjectById(projectId);
+
+		return project.tasks.find((task) => task.id === taskId);
 	}
 
 	static addTaskToProject(task, id) {
