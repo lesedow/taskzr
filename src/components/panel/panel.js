188b1fb0258cc9836f8de44e4d539cf25dcf9570
lesedow <lesedow@gmail.com>
2024-04-17 11:57:47 +0300

    refactor code

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,31 +1,67 @@
 import './panel.css';
 
-function Details(projects) {
-	const details1 = document.createElement('div');
-	details1.classList.add('details-1');
+function TaskTitleInput(value = '') {
+	const taskTitleInput = document.createElement('input');
+	taskTitleInput.type = 'text';
+	taskTitleInput.classList.add('task-panel-title');
+	taskTitleInput.placeholder = 'Title';
+	taskTitleInput.required = true;
+	taskTitleInput.name = 'title';
 
-	details1.innerHTML = `
-		<input type='text' id='new-task-title' placeholder='Title' required>
-		<textarea id='new-task-description' cols='30' rows='5' placeholder='Description'></textarea>
+	taskTitleInput.value = value;
+
+	return taskTitleInput;
+}
+
+function TaskDescription(value = '') {
+	const taskDescription = document.createElement('textarea');
+	taskDescription.classList.add('task-panel-description');
+	taskDescription.cols = 30;
+	taskDescription.rows = 5;
+	taskDescription.placeholder = 'Description';
+	taskDescription.required = true;
+	taskDescription.name = 'description';
+
+	taskDescription.value = value;
+
+	return taskDescription;
+}
+
+function TaskDate(value = '') {
+	const taskDate = document.createElement('input');
+	taskDate.type = 'date';
+	taskDate.name = 'date';
+
+	taskDate.required = true;
+
+	taskDate.value = value;
+
+	return taskDate;
+}
+
+function TaskPriority(value) {
+	const taskPriority = document.createElement('select');
+	taskPriority.name = 'priority';
+	// taskPriority.setAttribute('id', 'priority');
+
+	taskPriority.innerHTML = `
+		<option value="1">Priority 1</option>
+		<option value="2">Priority 2</option>
+		<option value="3">Priority 3</option>
+		<option value="4">Priority 4</option>
 	`;
 
-	const details2 = document.createElement('div');
-	details2.classList.add('details-2');
+	if (value !== undefined) {
+		taskPriority.value = value;
+	}
 
-	details2.innerHTML = `
-		<input type='date' id='new-task-date' required>
-		<select name='priority' id='priority'>
-			<option value="1">Priority 1</option>
-      <option value="2">Priority 2</option>
-      <option value="3">Priority 3</option>
-      <option value="4">Priority 4</option>
-		</select>
-	`;
+	return taskPriority;
+}
 
+function ProjectOptions(projects, value) {
 	const projectSelect = document.createElement('select');
-	projectSelect.name = "project-select";
+	projectSelect.name = 'projectID';
 	projectSelect.setAttribute('id', 'project-select');
-	details2.appendChild(projectSelect);
 
 	projects.forEach(project => {
 		const option = document.createElement('option');
@@ -35,36 +71,93 @@ function Details(projects) {
 		projectSelect.appendChild(option);
 	});
 
-	return [details1, details2]; 
+	if (value !== undefined) {
+		projectSelect.value = value;
+	}
+
+	return projectSelect;
 }
 
-export function NewTaskPanel(projects, func) {
+function Details(projects, task = {}) {
+	const details1 = document.createElement('div');
+	details1.classList.add('details-1');
+
+	details1.appendChild(TaskTitleInput(task.title));
+	details1.appendChild(TaskDescription(task.description));
+
+	const details2 = document.createElement('div');
+	details2.classList.add('details-2');
+
+	details2.appendChild(TaskDate(task.dueDate));
+	details2.appendChild(TaskPriority(task.priority));
+	details2.appendChild(ProjectOptions(projects, task.projectID));
+
+	return [details1, details2];
+}
+
+export function EditTaskPanel(projects, task, onSubmit, onRemove) {
+	const backdrop = document.createElement('div');
+	backdrop.classList.add('backdrop');
+
+	const editTaskContainer = document.createElement('div');
+	editTaskContainer.classList.add('task-panel');
+	backdrop.appendChild(editTaskContainer);
+
+	const editTaskForm = document.createElement('form');
+	editTaskForm.action = '#';
+	editTaskForm.setAttribute('id', 'task-form');
+	editTaskContainer.appendChild(editTaskForm);
+
+	const details = Details(projects, task);
+	editTaskForm.replaceChildren(...details);
+
+	const submitButton = document.createElement('button');
+	submitButton.type = 'submit';
+	submitButton.textContent = 'Edit Task';
+	editTaskForm.appendChild(submitButton);
+
+	const removeTaskButton = document.createElement('button');
+	removeTaskButton.textContent = 'Remove Task';
+	removeTaskButton.addEventListener('click', onRemove);
+	editTaskForm.appendChild(removeTaskButton);
+
+	editTaskContainer.addEventListener('submit', onSubmit);
+
+	return backdrop;
+}
+
+export function NewTaskPanel(projects, onSubmit) {
 	const backdrop = document.createElement('div');
 	backdrop.classList.add('backdrop');
 
 	const newTaskContainer = document.createElement('div');
-	newTaskContainer.classList.add('new-task');
+	newTaskContainer.classList.add('task-panel');
 	backdrop.appendChild(newTaskContainer);
 
 	const newTaskForm = document.createElement('form');
 	newTaskForm.action = '#';
-	newTaskForm.setAttribute('id', 'new-task-form');
+	newTaskForm.setAttribute('id', 'task-form');
 	newTaskContainer.appendChild(newTaskForm);
 
 	const details = Details(projects);
 	newTaskForm.replaceChildren(...details);
 
 	const submitButton = document.createElement('button');
+	submitButton.classList.add('span-2');
 	submitButton.type = 'submit';
 	submitButton.textContent = 'Add Task';
 	newTaskForm.appendChild(submitButton);
 
-	newTaskContainer.addEventListener('submit', func);
+	newTaskContainer.addEventListener('submit', onSubmit);
 
 	return backdrop;
 }
 
 export function NewProjectPanel() {
+
+}
+
+export function EditProjectPanel() {
 
 }
 
